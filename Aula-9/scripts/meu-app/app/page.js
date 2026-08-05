39fb983f71507fcb370a4afd68f6afb1b270b092
Guilherme Freire Pires <guiga_freire06@hotmail.com>
2026-08-05 13:29:48 -0300

    Atribuições em React

--- Aula-9/scripts/meu-app/app/page.js
+++ Aula-9/scripts/meu-app/app/page.js
@@ -1,13 +1,15 @@
 import Hello from './components/Hello';
 
+const nome = 'Guilherme';
+
 function Home() {
   return (
     <div>
-      <Hello nome="Maria" />
-      <Hello nome="João" />
-      <Hello nome="Eric" />
+      <Hello nome={nome} />
+      {nome === 'Guilherme' && <p>Que bom ver você aqui!</p>}
     </div>
   );
 }
 
 export default Home;
+
